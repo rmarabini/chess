a4d38101c4726525b4616571cb2557833e1dc04d
Your Name <you@example.com>
2021-01-12 10:27:16 +0100

    add chess notation

--- matrixled.py
+++ matrixled.py
@@ -11,6 +11,15 @@ from Adafruit_LED_Backpack import Matrix8x8
 """Class that controls an Adafruit HT16K33 16×8 LED Matrix Driver."""
 
 class HT16K33():
+    xMapper={}
+    yMapper = {}
+    for k, v in zip ([1, 2, 3, 4, 5, 6, 7, 8],
+                     [0, 1, 2, 3, 4, 5, 6, 7]):
+        yMapper[k]=v
+    for k, v in zip (['a','b', 'c','d','e','f','g','h'],
+                     [0, 1, 2, 3, 4, 5, 6, 7]):
+        xMapper[k]=v
+
     def __init__(self, address=0x70, busnum=1):
         """Create display instance on default I2C address (0x70) and bus number.
            check using I2cdetect -y 1  to make sure the address is 70,
@@ -20,8 +29,16 @@ class HT16K33():
         # Initialize the display. Must be called once before using the display.
         self.display.begin()
 
-    def setPixelsOn(self, xList, yList, clear = True):
-        """ x and y are arrays. light all the pixels in the array"""
+    def setPixelsOn(self, xList, yList, clear = True,
+                    chessMapperOn=False):
+        """ x and y are arrays. light all the pixels in the array.
+        If chessMapper is True then the input uses chess notation"""
+        if chessMapperOn:
+            for n, x in enumerate(xList):
+                xList[n] = self.xMapper[x]
+            for n, y in enumerate(yList):
+                yList[n] = self.yMapper[y.lower()]
+
         for x, y in zip(xList, yList):
             if clear:
                 self.display.clear()
@@ -30,8 +47,16 @@ class HT16K33():
             self.display.set_pixel(x+1, y, 1)
             self.display.write_display()
 
-    def setPixelsOff(self, xList, yList, clear = True):
-        """ x and y are arrays. light all the pixels in the array"""
+    def setPixelsOff(self, xList, yList, clear = True,
+                     chessMapperOn=False):
+        """ x and y are arrays. light all the pixels in the array
+            If chessMapper is True then the input uses chess notation"""
+        if chessMapperOn:
+            for n, x in enumerate(xList):
+                xList[n] = self.xMapper[x]
+            for n, y in enumerate(yList):
+                yList[n] = self.yMapper[y.lower()]
+
         for x, y in zip(xList, yList):
             if clear:
                 self.display.clear()
@@ -57,15 +82,28 @@ class HT16K33():
         while True:
             for led in range(size*size):
                 x = led // 3
-                y = led % 3   # cathodes number start 0
+                y = led % 3
                 self.setPixelsOn([x], [y], clear = False)
                 if printOn:
                     print("led %d %d ON" % (x, y))
                 time.sleep(seconds)
             for led in range(size*size):
                 x = led // 3
-                y = led % 3   # cathodes number start 0
+                y = led % 3
                 self.setPixelsOff([x], [y], clear = False)
                 if printOn:
                     print("led %d %d ON" % (x, y))
+                time.sleep(seconds)
+
+    def testMatrix3(self, size=8, printOn=True, seconds=1):
+        """test function. LEDS light one by one in the order 1 to size*size"""
+        xx=[1,2,3,4,5,6,7,8]
+        yy=['a','b','c','d','e','f','g','h']
+        while True:
+            for led in range(size*size):
+                x = xx[led // 3]
+                y = yy[led % 3]
+                self.setPixelsOn([x], [y], clear = True, chessMapperOn=True)
+                if printOn:
+                    print("led %d %s ON" % (x, y))
                 time.sleep(seconds)
