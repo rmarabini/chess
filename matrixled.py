0986c731d7f6c5fb8ba1afa13b9987e4b211e25d
Your Name <you@example.com>
2021-01-15 10:29:09 +0100

    add matrix and print

--- matrixled.py
+++ matrixled.py
@@ -15,12 +15,21 @@ class HT16K33():
     """Class that controls an Adafruit HT16K33 16×8 LED Matrix Driver."""
     xMapper={}
     yMapper = {}
+    xMapperT={}
+    yMapperT = {}
     for k, v in zip ([1, 2, 3, 4, 5, 6, 7, 8],
                      [0, 1, 2, 3, 4, 5, 6, 7]):
         yMapper[k]=v
     for k, v in zip (['a','b', 'c','d','e','f','g','h'],
                      [0, 1, 2, 3, 4, 5, 6, 7]):
         xMapper[k]=v
+    for k, v in zip ([0, 1, 2, 3, 4, 5, 6, 7],
+                     [1, 2, 3, 4, 5, 6, 7, 8]
+                     ):
+        yMapperT[k]=v
+    for k, v in zip ([0, 1, 2, 3, 4, 5, 6, 7],
+                     ['a','b', 'c','d','e','f','g','h']):
+        xMapperT[k]=v
 
     def __init__(self, address=0x70, busnum=1, size=8):
         """Create display instance on default I2C address (0x70) and bus number.
@@ -98,7 +107,8 @@ class HT16K33():
                 y = led % 3   # cathodes number start 0
                 self.setPixelsOn([x], [y], clear = True)
                 if printOn:
-                    print("led %d %d ON" % (x, y))
+                    print("led %d %d ON" %
+                          (self.xMapperT[x], self.xMapperT[y]))
                     self.print()
                 time.sleep(seconds)
 
