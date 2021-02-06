e6dbb6fdb6d6037476e54196522bd8cb1dac6c63
Your Name <you@example.com>
2021-02-06 12:07:22 +0100

    w

--- tester.py
+++ tester.py
@@ -1,4 +1,21 @@
 from matrixled import HT16K33
 
-ledMatrix = HT16K33()
-ledMatrix.testMatrix(size=3, printOn=True, seconds=0.5)
+ledMatrix = HT16K33(size=3)
+ledMatrix.testMatrix(printOn=True, seconds=0.5)
+#ledMatrix.testMatrix2(printOn=True, seconds=0.5)
+
+##########
+#>>> from gpioreed import GPIOreed
+#>>> g = GPIOreed(size=1)
+#>>> g.print()
+# 1 [0 ]
+#    a
+#>>> g.getGPIOMatrix()
+#>>> g.print()
+#######
+#>>> from gpioreed import GPIOreed; g = GPIOreed(size=3)
+#>>> g.getGPIOMatrix(); g.print()
+#>>> from matrixled import HT16K33
+#>>> ledMatrix = HT16K33(size=3)
+#>>> g.getGPIOMatrix(ledMatrix); g.print()
+
