2ebb358a71efaf891fec5c5538db1ece55f18285
Your Name <you@example.com>
2021-02-13 19:48:15 +0100

    w

--- constants.py
+++ constants.py
@@ -2,11 +2,11 @@ xMapper = {}
 yMapper = {}
 xMapperT = {}
 yMapperT = {}
-for k, v in zip([1, 2, 3, 4, 5, 6, 7, 8],
-                [0, 1, 2, 3, 4, 5, 6, 7]):
+for k, v in zip([1, 2, 3, 4, 5, 6, 7, 8, 9],
+                [0, 1, 2, 3, 4, 5, 6, 7, 9]):
     yMapper[k] = v
-for k, v in zip(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
-                [0, 1, 2, 3, 4, 5, 6, 7]):
+for k, v in zip(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'],
+                [0, 1, 2, 3, 4, 5, 6, 7, 8]):
     xMapper[k] = v
 for k, v in zip([0, 1, 2, 3, 4, 5, 6, 7],
                 [1, 2, 3, 4, 5, 6, 7, 8]
--- tester.py
+++ tester.py
@@ -1,6 +1,6 @@
 from matrixled import HT16K33
 
 ledMatrix = HT16K33()
-ledMatrix.testMatrix(size=3, printOn=True, seconds=0.5)
+ledMatrix.testMatrix(ledMatrix, size=3, printOn=True, seconds=0.5)
 
 
