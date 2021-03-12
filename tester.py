c165360d2897b1ab17bd9b7aed006f6d1e044ae1
Your Name <you@example.com>
2021-03-12 15:45:57 +0100

    allow user to select test

--- tester.py
+++ tester.py
@@ -1,8 +1,33 @@
-from matrixled import HT16K33, testMatrix, testMatrix2
-from gpioreed import CPIOreed
-cpio = CPIOreed(address=0x21, size=3)
-cpio.reset2()
-cpio.testUsingReeds()
+from matrixled import HT16K33, testMatrix, testMatrix2, testMatrix3
+from gpioreed import CPIOreed, testUsingReeds, testUsingReeds2
+from reedled import testLedReedCloseMatrix, testLedReedCloseSwitch
+import sys
+import time
+
+arg = sys.argv[1]
+
+if arg == "led1":
+    ledMatrix = HT16K33(size=8)
+    testMatrix(ledMatrix, seconds = 0.25)
+elif arg == "led2":
+    ledMatrix = HT16K33(size=8)
+    testMatrix2(ledMatrix, seconds = 0.1)
+elif arg == "led3":
+    ledMatrix = HT16K33(size=8)
+    testMatrix3(ledMatrix)
+elif arg == "reed1":
+    cpio = CPIOreed(address=0x21, size=8)
+    cpio.reset2()
+    testUsingReeds(cpio)
+elif arg == "reed2":
+    cpio = CPIOreed(address=0x21, size=8)
+    cpio.reset2()
+    testUsingReeds2(cpio,seconds=0.5)
+elif arg == "both1":
+    testLedReedCloseMatrix(seconds=0.1)
+elif arg == "both2":
+    testLedReedCloseSwitch(seconds=1)
+    
 
 exit(1)
 """
@@ -45,9 +70,10 @@ cpio = CPIOreed(address=0x21, size=3)
 cpio.testUsingLeds()
 
 
-from matrixled import HT16K33, testMatrix, testMatrix2
-ledMatrix = HT16K33(size=9)
-testMatrix2(ledMatrix, True, .5)
+from matrixled import HT16K33, testMatrix, testMatrix2, testMatrix3
+ledMatrix = HT16K33(size=8)
+testMatrix3(ledMatrix)
+#testMatrix2(ledMatrix, True, .25)
 
 
 ##########
