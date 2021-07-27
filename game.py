72bcd67286b769396e66534b2a6c1884f905ac90
Your Name <you@example.com>
2021-07-27 11:31:51 +0200

    test sonarcloud

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ import numpy as np
 from datetime import datetime
 class BoardError(Exception):
     pass
-import os
+import os, time
 
 class Chess():
     """ Main class for chess game. pip3 install chess required"""
